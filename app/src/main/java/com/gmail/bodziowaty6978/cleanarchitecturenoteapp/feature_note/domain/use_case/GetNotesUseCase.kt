fcc2e620ee04fb5cd0e40fd0d3bbc27019394325
Bodzio6978 <Bodziowaty6978@gmail.com>
2022-04-07 16:49:20 +0200

    added test cases for sorting notes

--- app/src/main/java/com/gmail/bodziowaty6978/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNotesUseCase.kt
+++ app/src/main/java/com/gmail/bodziowaty6978/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNotesUseCase.kt
@@ -1,6 +1,5 @@
 package com.gmail.bodziowaty6978.cleanarchitecturenoteapp.feature_note.domain.use_case
 
-import androidx.compose.ui.text.toLowerCase
 import com.gmail.bodziowaty6978.cleanarchitecturenoteapp.feature_note.domain.model.Note
 import com.gmail.bodziowaty6978.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
 import com.gmail.bodziowaty6978.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
@@ -9,22 +8,21 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class GetNotesUseCase(
-    private val repository:NoteRepository
+    private val repository: NoteRepository
 ) {
 
-    operator fun invoke(noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending)): Flow<List<Note>>{
+    operator fun invoke(noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending)): Flow<List<Note>> {
         return repository.getNotes().map { notes ->
-            when(noteOrder.orderType){
+            when (noteOrder.orderType) {
                 is OrderType.Ascending -> {
-                    when(noteOrder){
+                    when (noteOrder) {
                         is NoteOrder.Title -> notes.sortedBy { it.title.toLowerCase() }
                         is NoteOrder.Date -> notes.sortedBy { it.timestamp }
                         is NoteOrder.Color -> notes.sortedBy { it.color }
                     }
-
                 }
                 is OrderType.Descending -> {
-                    when(noteOrder){
+                    when (noteOrder) {
                         is NoteOrder.Title -> notes.sortedByDescending { it.title.toLowerCase() }
                         is NoteOrder.Date -> notes.sortedByDescending { it.timestamp }
                         is NoteOrder.Color -> notes.sortedByDescending { it.color }
